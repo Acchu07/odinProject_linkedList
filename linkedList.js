0ec96a84d26bc5e46ac33a2fd8134f8b753ffaeb
Acchu <acchu1915@gmail.com>
2024-03-10 10:06:02 +0530

    linked list: help on odin project for head

---
+++ linkedList.js
@@ -0,0 +1,42 @@
+class Node{
+    constructor(data,next){
+        this.data = data;
+        this.next = null;
+    }
+}
+
+
+class LinkedList{
+    
+    static append(nodeObject,nodeObjectAdd){
+        nodeObject.next = nodeObjectAdd;
+    }
+
+    static prepend(nodeObjectNewHead,nodeObjectCurrentHead){
+        nodeObjectNewHead.next = nodeObjectCurrentHead;
+    }
+
+    static size(nodeObject){
+        if(nodeObject.next === null){
+            return 1;
+        }
+        let size = 1;
+        while(nodeObject.next !== null){
+            nodeObject = nodeObject.next;
+            size++;
+        }
+        return size;
+    }
+}
+
+
+const nodeHead = new Node(10);
+const nodeBody1 = new Node(20);
+const nodeBody2 = new Node(50);
+
+console.log(nodeHead);
+
+LinkedList.append(nodeHead, nodeBody1);
+
+console.log(nodeHead);
+console.log(LinkedList.size(nodeHead));
